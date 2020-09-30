4902292bf1d4dc8c68c9683626c779f2345a3dbe
Victor Moraes <victordlmoraes@hotmail.com>
2020-09-30 14:40:13 -0300

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -6,21 +6,28 @@ function iniciarContagem() {
     let pulo = document.getElementById('numPulo')
     let res = document.getElementById('res')
 
-    if(numInicial.length == 0 || numFinal.length == 0 || numPulo == 0) {
-        alert("[ERRO] Dados em falta!")
+    if(ini.value.length == 0 || fim.value.length == 0 || pulo.value.length == 0) {
+        res.innerHTML = 'Impossível contar!';
+        alert("[ERRO] Dados em falta!");
     } else {
+        res.innerHTML = 'Contando... '
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(pulo.value)
-        res.innerHTML = 'Contando... '
+        if(p <= 0) {
+            alert("[ERRO] Pulo inválido! Considerando o pulo como 1.");
+            p = 1;
+        }
         if(i < f) {
+            // Realização da contagem crescente
             for(let c = i; c <= f; c += p) {
                 res.innerHTML += `${c} `
             }
         } else {
+            // Realização da contagem decrescente
             for(let c = i; c >= f; c -= p) {
                 res.innerHTML += `${c} `
             }
         }
     }
-}
+};
